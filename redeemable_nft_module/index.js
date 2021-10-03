1d6f6c50549e3cb0e28a571a7d5a4a2a0c952ff1
Aldo Suhartono Putra <aldhosutra@gmail.com>
2021-10-03 17:39:26 +0700

    afterBlockApply to monitor redeem status

--- redeemable_nft_module/index.js
+++ redeemable_nft_module/index.js
@@ -5,11 +5,14 @@ const MintNFTAsset = require("./assets/mint_nft");
 const RejectRedeemAsset = require("./assets/reject_redeem");
 const RequestRedeemAsset = require("./assets/request_redeem");
 const { REDEEMABLE_NFT_MODULE_ID, REDEEMABLE_NFT_REQUEST_REDEEM, REDEEMABLE_NFT_DELIVER_REDEEM, REDEEMBALE_NFT_REJECT_REDEEM } = require("./constants/id");
+const JOURNEYACTIVITY = require("./constants/journey_activity");
+const REDEEMSTATUS = require("./constants/redeem_status");
 const { RedeemableNFTAccountSchema } = require("./schemas/account");
 const DeliverRedeemAssetSchema = require("./schemas/assets/deliever_redeem");
 const RejectRedeemAssetSchema = require("./schemas/assets/reject_redeem");
 const RequestRedeemAssetSchema = require("./schemas/assets/request_redeem");
-const { getNFTById } = require("./utils/chain_state");
+const { getNFTById, getRedeemMonitor, setNFTById, setRedeemMonitor } = require("./utils/chain_state");
+const { asyncForEach } = require("./utils/helper");
 
 class RedeemableNFTModule extends BaseModule {
   name = "redeemableNFT";
@@ -48,6 +51,56 @@ class RedeemableNFTModule extends BaseModule {
         nft: nft.id.toString("hex"),
         to: nft.originAddress.toString("hex"),
       });
+    }
+  }
+
+  async afterBlockApply({ block, stateStore, reducerHandler, consensus }) {
+    const redeemMonitor = await getRedeemMonitor(stateStore);
+    if (redeemMonitor.checkpoint !== 0 && redeemMonitor.checkpoint < block.header.timestamp) {
+      const index = [];
+      for (let i = 0; i < redeemMonitor.all.length; i++) {
+        if (redeemMonitor.all[i].time >= block.header.timestamp) break;
+        else index.push(i);
+      }
+      asyncForEach(index, (i) => {
+        const nft = await getNFTById(stateStore, redeemMonitor.all[i].nft);
+        nft.redeem.status = REDEEMSTATUS.IDLE;
+        switch (redeemMonitor.all[i].status) {
+          case REDEEMSTATUS.REQUESTED:
+            const originAccount = await stateStore.account.get(nft.originAddress);
+            const senderRequestIndex = originAccount.redeemableNFT.creator.requestQueue.findIndex((a) => a.equals(nft.id));
+            originAccount.redeemableNFT.creator.timeout++;
+            originAccount.redeemableNFT.creator.requestQueue.splice(senderRequestIndex, 1);
+            await stateStore.account.set(nft.originAddress, originAccount);
+            nft.journey.unshift({
+              activity: JOURNEYACTIVITY.TIMEOUT,
+              date: block.header.timestamp,
+              by: "",
+              from: nft.originAddress,
+              message: `Requested by ${nft.ownerAddress.toString("hex")} on: ${new Date(nft.redeem.touched).toUTCString()}`,
+              amount: 0,
+            });
+            break;
+          case REDEEMSTATUS.READY:
+            nft.redeem.message.cipher = "";
+            nft.redeem.message.nonce = "";
+            break;
+          default:
+            throw new Error("FATAL: [afterBlockApply at redeemableNFT module] unkown status");
+        }
+        nft.redeem.touched = block.header.timestamp;
+        await setNFTById(stateStore, nft.id, nft);
+      });
+      for (let i = index.length - 1; i >= 0; i--) redeemMonitor.all.splice(i, 1);
+      if (redeemMonitor.all.length > 0) {
+        redeemMonitor.all.sort((a, b) => a - b);
+        if (redeemMonitor.checkpoint > redeemMonitor.all[0].time) {
+          redeemMonitor.checkpoint = redeemMonitor.all[0].time;
+        }
+      } else {
+        redeemMonitor.checkpoint = 0;
+      }
+      await setRedeemMonitor(stateStore, redeemMonitor);
     }
   }
 }
