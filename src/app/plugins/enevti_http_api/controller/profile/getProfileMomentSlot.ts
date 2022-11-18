eeddd456a6d99c43a3dac13c0a71a5a14405e216
Aldo Suhartono Putra <aldhosutra@gmail.com>
2022-11-19 03:49:09 +0700

    get profile moment slot optimization

--- src/app/plugins/enevti_http_api/controller/profile/getProfileMomentSlot.ts
+++ src/app/plugins/enevti_http_api/controller/profile/getProfileMomentSlot.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 import { BaseChannel } from 'lisk-framework';
-import { NFT } from '../../../../../types/core/chain/nft';
+import { NFTBase } from '../../../../../types/core/chain/nft';
 import { invokeGetAccountStats } from '../../utils/invoker/redeemable_nft_module';
 import idBufferToNFT from '../../utils/transformer/idBufferToNFT';
+import { minimizeNFT } from '../../utils/transformer/minimizeToBase';
 import { validateAddress } from '../../utils/validation/address';
 
 export default (channel: BaseChannel) => async (req: Request, res: Response) => {
@@ -11,12 +12,12 @@ export default (channel: BaseChannel) => async (req: Request, res: Response) => 
     validateAddress(address);
     const accountStats = await invokeGetAccountStats(channel, address);
 
-    const ret: NFT[] = await Promise.all(
+    const ret: NFTBase[] = await Promise.all(
       accountStats.momentSlot.map(
-        async (item): Promise<NFT> => {
+        async (item): Promise<NFTBase> => {
           const nft = await idBufferToNFT(channel, item);
           if (!nft) throw new Error('NFT not found while iterating momentSlot');
-          return nft;
+          return minimizeNFT(nft);
         },
       ),
     );
