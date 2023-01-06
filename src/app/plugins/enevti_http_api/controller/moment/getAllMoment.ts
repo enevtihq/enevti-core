1c284e6234cd1c990042bbe0495de55670e41173
Aldo Suhartono Putra <aldhosutra@gmail.com>
2023-01-07 00:13:59 +0700

    fix getAllMoment endpoint to add pagination data

--- src/app/plugins/enevti_http_api/controller/moment/getAllMoment.ts
+++ src/app/plugins/enevti_http_api/controller/moment/getAllMoment.ts
@@ -1,27 +1,24 @@
 import { Request, Response } from 'express';
 import { BaseChannel } from 'lisk-framework';
-import { Moment } from '../../../../../types/core/chain/moment';
+import { MomentBase } from '../../../../../types/core/chain/moment';
 import { invokeGetAllMoment, invokeGetLiked } from '../../utils/invoker/redeemable_nft_module';
+import createPagination from '../../utils/misc/createPagination';
 import idBufferToMoment from '../../utils/transformer/idBufferToMoment';
 
 export default (channel: BaseChannel) => async (req: Request, res: Response) => {
   try {
     const { offset, limit, version, viewer } = req.query as Record<string, string>;
-    const moments = await invokeGetAllMoment(
-      channel,
-      offset ? parseInt(offset, 10) : undefined,
-      limit ? parseInt(limit, 10) : undefined,
-      version ? parseInt(version, 10) : undefined,
-    );
+    const moments = await invokeGetAllMoment(channel);
+    const { v, o, c } = createPagination(moments.data.length, version, offset, limit);
 
-    const response: (Moment & { liked: boolean })[] = await Promise.all(
-      moments.data.map(
-        async (item): Promise<Moment & { liked: boolean }> => {
+    const momentList: MomentBase[] = await Promise.all(
+      moments.data.slice(o, c).map(
+        async (item): Promise<MomentBase> => {
+          const data = await idBufferToMoment(channel, item.id);
+          if (!data) throw new Error('Error while iterating allMoments.data');
           const liked = viewer
             ? (await invokeGetLiked(channel, item.id.toString('hex'), viewer)) === 1
             : false;
-          const data = await idBufferToMoment(channel, item.id);
-          if (!data) throw new Error('Error while iterating allMoments.data');
           return {
             ...data,
             liked,
@@ -29,6 +26,12 @@ export default (channel: BaseChannel) => async (req: Request, res: Response) => 
         },
       ),
     );
+
+    const response = {
+      checkpoint: c,
+      version: v,
+      data: momentList,
+    };
 
     res.status(200).json({ data: response, meta: req.query });
   } catch (err: unknown) {
