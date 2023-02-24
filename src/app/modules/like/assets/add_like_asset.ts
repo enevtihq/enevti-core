11326313a7825f1e8ec0db1e5f941983750a0c3b
Aldo Suhartono Putra <aldhosutra@gmail.com>
2023-02-24 20:36:12 +0700

    add test to add_like_asset, coverage like module 100%

--- src/app/modules/like/assets/add_like_asset.ts
+++ src/app/modules/like/assets/add_like_asset.ts
@@ -1,9 +1,10 @@
 import { AddLikeProps } from 'enevti-types/asset/like/add_like_asset';
 import { BaseAsset, ApplyAssetContext, ValidateAssetContext } from 'lisk-sdk';
-import { ADD_LIKE_ASSET_NAME, LIKE_MODULE_PREFIX } from '../constants/codec';
+import { ADD_LIKE_ASSET_NAME } from '../constants/codec';
 import { ADD_LIKE_ASSET_ID } from '../constants/id';
 import { IDENTIFIER_MAX_LENGTH, ID_MAX_LENGTH } from '../constants/limit';
 import { addLikeSchema } from '../schema/assets/add_like_assets';
+import { addLike } from '../utils/add';
 
 export class AddLikeAsset extends BaseAsset {
   public name = ADD_LIKE_ASSET_NAME;
@@ -20,15 +21,17 @@ export class AddLikeAsset extends BaseAsset {
   }
 
   public async apply({
+    stateStore,
     asset,
     transaction,
     reducerHandler,
   }: ApplyAssetContext<AddLikeProps>): Promise<void> {
-    await reducerHandler.invoke(`${LIKE_MODULE_PREFIX}:addLike`, {
+    await addLike(
+      stateStore,
       reducerHandler,
-      identifier: asset.identifier,
-      target: asset.id,
-      senderAddress: transaction.senderAddress,
-    });
+      asset.identifier,
+      asset.id,
+      transaction.senderAddress,
+    );
   }
 }
--- test/unit/modules/like/assets/add_like_asset.spec.ts
+++ test/unit/modules/like/assets/add_like_asset.spec.ts
@@ -1,40 +1,202 @@
+import { AddLikeProps } from 'enevti-types/asset/like/add_like_asset';
+import { LikeChain, LikedChain } from 'enevti-types/chain/like';
+import { codec, ReducerHandler, StateStore, testing } from 'lisk-sdk';
+import { COUNT_PREFIX } from '../../../../../src/app/modules/count/constants/codec';
 import { AddLikeAsset } from '../../../../../src/app/modules/like/assets/add_like_asset';
+import {
+  ADD_LIKE_ASSET_NAME,
+  LIKED_PREFIX,
+  LIKE_MODULE_PREFIX,
+  LIKE_PREFIX,
+} from '../../../../../src/app/modules/like/constants/codec';
+import { ADD_LIKE_ASSET_ID } from '../../../../../src/app/modules/like/constants/id';
+import {
+  IDENTIFIER_MAX_LENGTH,
+  ID_MAX_LENGTH,
+} from '../../../../../src/app/modules/like/constants/limit';
+import { addLikeSchema } from '../../../../../src/app/modules/like/schema/assets/add_like_assets';
+import { likeSchema } from '../../../../../src/app/modules/like/schema/like';
+import { likedSchema } from '../../../../../src/app/modules/like/schema/liked';
+import { getLike } from '../../../../../src/app/modules/like/utils/like';
+import { getLiked } from '../../../../../src/app/modules/like/utils/liked';
 
 describe('AddLikeAsset', () => {
   let transactionAsset: AddLikeAsset;
 
-	beforeEach(() => {
-		transactionAsset = new AddLikeAsset();
-	});
+  beforeEach(() => {
+    transactionAsset = new AddLikeAsset();
+  });
 
-	describe('constructor', () => {
-		it('should have valid id', () => {
-			expect(transactionAsset.id).toEqual(0);
-		});
-
-		it('should have valid name', () => {
-			expect(transactionAsset.name).toEqual('addLike');
-		});
-
-		it('should have valid schema', () => {
-			expect(transactionAsset.schema).toMatchSnapshot();
-		});
-	});
-
-	describe('validate', () => {
-		describe('schema validation', () => {
-      it.todo('should throw errors for invalid schema');
-      it.todo('should be ok for valid schema');
-    });
-	});
-
-	describe('apply', () => {
-    describe('valid cases', () => {
-      it.todo('should update the state store');
+  describe('constructor', () => {
+    it('should have valid id', () => {
+      expect(transactionAsset.id).toEqual(ADD_LIKE_ASSET_ID);
     });
 
-    describe('invalid cases', () => {
-      it.todo('should throw error');
+    it('should have valid name', () => {
+      expect(transactionAsset.name).toEqual(ADD_LIKE_ASSET_NAME);
     });
-	});
+
+    it('should have valid schema', () => {
+      expect(transactionAsset.schema).toEqual(addLikeSchema);
+    });
+  });
+
+  describe('validate', () => {
+    describe('schema validation', () => {
+      it('should throw error if identifier length exceeding limit', () => {
+        const context = testing.createValidateAssetContext<AddLikeProps>({
+          asset: { identifier: 'a'.repeat(IDENTIFIER_MAX_LENGTH + 1), id: 'id' },
+          transaction: { senderAddress: Buffer.alloc(0) } as any,
+        });
+        expect(() => transactionAsset.validate(context)).toThrow();
+      });
+
+      it('should throw error if id length exceeding limit', () => {
+        const context = testing.createValidateAssetContext<AddLikeProps>({
+          asset: { identifier: 'identifier', id: 'a'.repeat(ID_MAX_LENGTH + 1) },
+          transaction: { senderAddress: Buffer.alloc(0) } as any,
+        });
+        expect(() => transactionAsset.validate(context)).toThrow();
+      });
+
+      it('should be ok for valid schema', () => {
+        const context = testing.createValidateAssetContext<AddLikeProps>({
+          asset: { id: 'id', identifier: 'identifier' },
+          transaction: { senderAddress: Buffer.alloc(0) } as any,
+        });
+        expect(() => transactionAsset.validate(context)).not.toThrow();
+      });
+    });
+  });
+
+  describe('apply', () => {
+    let stateStore: StateStore;
+    let context;
+
+    const reducerHandler: ReducerHandler = testing.mocks.reducerHandlerMock;
+    const identifier = 'identifier';
+    const target = 'target';
+    const { address } = testing.fixtures.defaultFaucetAccount;
+    const likeChain: LikeChain = {
+      address: [address],
+    };
+    const likedChain: LikedChain = {
+      status: 1,
+    };
+    const likeChainValue = codec.encode(likeSchema, likeChain);
+    const likedChainValue = codec.encode(likedSchema, likedChain);
+
+    beforeEach(() => {
+      const chain = {
+        [`${LIKE_MODULE_PREFIX}:${identifier}:${target}:${LIKE_PREFIX}`]: likeChainValue,
+        [`${LIKE_MODULE_PREFIX}:${LIKED_PREFIX}:${target}:${address.toString(
+          'hex',
+        )}`]: likedChainValue,
+      };
+
+      stateStore = new testing.mocks.StateStoreMock({
+        chain,
+      });
+
+      context = testing.createApplyAssetContext<AddLikeProps>({
+        stateStore,
+        reducerHandler,
+        asset: { identifier, id: target },
+        transaction: { senderAddress: address, nonce: BigInt(1) } as any,
+      });
+
+      jest.spyOn(reducerHandler, 'invoke');
+      jest.spyOn(stateStore.chain, 'get');
+      jest.spyOn(stateStore.chain, 'set');
+    });
+
+    it('should add like to blockchain state', async () => {
+      context = testing.createApplyAssetContext<AddLikeProps>({
+        stateStore,
+        reducerHandler,
+        asset: { identifier: 'newIdentifier', id: 'newTarget' },
+        transaction: { senderAddress: address, nonce: BigInt(1) } as any,
+      });
+
+      await transactionAsset.apply(context);
+
+      const like = await getLike(stateStore, context.asset.identifier, context.asset.id);
+      expect(like).toEqual(likeChain);
+    });
+
+    it('should throw an error if sender already give a like', async () => {
+      const func = async () => {
+        try {
+          await transactionAsset.apply(context);
+          return true;
+        } catch {
+          return false;
+        }
+      };
+      expect(await func()).toBe(false);
+    });
+
+    it('should set liked to 1 if sender has not been give a like', async () => {
+      context = testing.createApplyAssetContext<AddLikeProps>({
+        stateStore,
+        reducerHandler,
+        asset: { identifier: 'newIdentifier', id: 'newTarget' },
+        transaction: { senderAddress: address, nonce: BigInt(1) } as any,
+      });
+
+      await transactionAsset.apply(context);
+
+      const liked = await getLiked(stateStore, context.asset.id, context.transaction.senderAddress);
+      expect(liked).toEqual({ status: 1 });
+    });
+
+    it('should unshift new like target data to blockchain', async () => {
+      const senderAddress = Buffer.from('newAddress', 'utf-8');
+      context = testing.createApplyAssetContext<AddLikeProps>({
+        stateStore,
+        reducerHandler,
+        asset: { identifier, id: target },
+        transaction: { senderAddress, nonce: BigInt(1) } as any,
+      });
+
+      await transactionAsset.apply(context);
+
+      const like = await getLike(stateStore, context.asset.identifier, context.asset.id);
+      expect(like).toEqual({ address: [senderAddress, ...likeChain.address] });
+    });
+
+    it('should only contain new target data for the first time', async () => {
+      const senderAddress = Buffer.from('newAddress', 'utf-8');
+      context = testing.createApplyAssetContext<AddLikeProps>({
+        stateStore,
+        reducerHandler,
+        asset: { identifier: 'newIdentifier', id: 'newTarget' },
+        transaction: { senderAddress, nonce: BigInt(1) } as any,
+      });
+
+      await transactionAsset.apply(context);
+
+      const like = await getLike(stateStore, context.asset.identifier, context.asset.id);
+
+      expect(like).toEqual({ address: [senderAddress] });
+    });
+
+    it('should invoke count module addCount reducers for succesful add like operation', async () => {
+      context = testing.createApplyAssetContext<AddLikeProps>({
+        stateStore,
+        reducerHandler,
+        asset: { identifier: 'newIdentifier', id: 'newTarget' },
+        transaction: { senderAddress: address, nonce: BigInt(1) } as any,
+      });
+
+      await transactionAsset.apply(context);
+
+      expect(reducerHandler.invoke).toHaveBeenCalledWith(`${COUNT_PREFIX}:addCount`, {
+        module: LIKE_MODULE_PREFIX,
+        key: 'newIdentifier',
+        address,
+        item: 'newTarget',
+      });
+    });
+  });
 });
